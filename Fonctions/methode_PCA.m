688adc7f2fa6ff1b211d2aa28cf30226214c5fbf
JustineLer <justine.leraut@free.fr>
2017-04-26 12:35:45 +0200

    changement de fonction
    "princomp" en "pca"

--- Fonctions/methode_PCA.m
+++ Fonctions/methode_PCA.m
@@ -5,7 +5,7 @@ fe     = fichier.ips;
 faxis  = (-fe/2:fe/taille:fe/2 - fe/taille);
 
 
-[COEFF,SCORE] = princomp(fichier.sig);
+[COEFF,SCORE] = pca(fichier.sig);
 s = SCORE(:,2);
 
 DSP = fftshift(abs(fft(s, taille)));
